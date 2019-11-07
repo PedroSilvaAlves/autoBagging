aacc69231c7915d56541a6424446fd52ccc9c94e
PedroSilvaAlves <pedrosilvaalves@hotmail.com>
2019-11-07 17:15:41 +0000

    Init project

---
+++ autoBagging.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+class autoBagging:
+
+    def __init__(type):
+        if type == 'create':
+            self.type == type
+        if type == 'predict':
+            self.type == type
+        else:
+            self.type == error
+    
+    def create(FileNameDataset, TargetsNames, Metafunctions, PostProcessing):
+        # Por cada file abrir o csv e tirar para um nparray
+
+    def predict(form, data):
+        if data[form].ntype != np.string
