559b60ceed3a490c39e13900a41f75746573eb50
PedroSilvaAlves <pedrosilvaalves@hotmail.com>
2019-11-11 12:23:42 +0000

    Bagging model with Base algorithms and params

--- autoBaggingClassifier.py
+++ autoBaggingClassifier.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pd
+import xgboost as xgb
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.dummy import DummyClassifier
+from sklearn.model_selection import ParameterGrid
 from metafeatures.core.object_analyzer import analyze_pd_dataframe
 from metafeatures.meta_functions.entropy import Entropy
 from metafeatures.meta_functions import basic as basic_meta_functions
@@ -13,12 +20,21 @@ from metafeatures.core.engine import metafeature_generator
 
 class autoBaggingClassifier:
 
+    base_estimators = {    'Naive Bayes': GaussianNB(),
+                           'Decision Tree (max_depth=1)': DecisionTreeClassifier(max_depth=1),
+                           'Decision Tree (max_depth=2)': DecisionTreeClassifier(max_depth=2),
+                           'Decision Tree (max_depth=3)': DecisionTreeClassifier(max_depth=3),
+                           'Majority Class': DummyClassifier()}
+    grid = ParameterGrid({"n_estimators" : [50,100,200],
+                           "bootstrap" : [True, False]
+                         })
+                          
     def __init__(self,state):
         if state == 'create' or state  == 'predict':
             self.type = state
         else:
             self.type = 'error'
-    
+
     def fit(self,
             file_name_datasets,
             target_names,
@@ -26,19 +42,48 @@ class autoBaggingClassifier:
             meta_functions_numerical,
             post_processing_steps):
         # Por cada file abrir o csv e tirar para um array de DataFrames
-        
+        self.datasets = []
+        self.bagging_workflows = []
         if self.type == 'create':
             meta_features = []
             for file_name, target in zip(file_name_datasets, target_names):
                 print(file_name)
                 dataset = pd.read_csv(file_name)
+                self.datasets.append(dataset)
                 if self._validateDataset(dataset,target):
-                    meta_features.append(self._metafeatures(dataset,[target],meta_functions_categorical,meta_functions_numerical,post_processing_steps))
-                
+                    # MetaFeatures
+                    meta_features_estematic = self._metafeatures(dataset,[target],meta_functions_categorical,meta_functions_numerical,post_processing_steps)
+                    meta_features_estematic['Number of Examples'] = dataset.shape[0]
+                    meta_features_estematic['Number of Features'] = dataset.shape[1]
+                    meta_features_estematic['Number of Classes'] = dataset[target].unique().shape[0]
+                    # Falta as previções
+                    # Então
+                    # Criar os modelos base
+
+                    for params in self.grid: # Combinações de Parametros
+                        # Criar uma Copia das metafeatures
+                        meta_features_final = meta_features_estematic.copy()
+                        for name, base_estimator in self.base_estimators: # em cada combinação dos parametros adiciona uma feature por algoritmo base
+                            # Criar modelo
+                            bagging_workflow = BaggingClassifier(base_estimator=base_estimator,
+                                              random_state=0,
+                                              **params)
+                            # Adicionar a lista de Workflows
+                            self.bagging_workflows.append(bagging_workflow)
+
+                            # adicionar ao array, uma previção do algoritmo atual                            
+                            meta_features_final['Landmark: ' + name] # = bagging_workflow.score() ou cross validation
+
+                        meta_features.append(meta_features_final)   # Este array contem as metafeatures criadas e as previções dos algoritmos base
+
+                    #Fit & Validate
+                    
+                    
+
+                    
             self.meta_data = pd.DataFrame(meta_features)
             self.meta_data.to_csv('meta_data.csv')
             print(self.meta_data)
-
 
     def _metafeatures(self, dataset,target,meta_functions_categorical,meta_functions_numerical,post_processing_steps):
             
@@ -90,8 +135,8 @@ FileNameDataset.append('./datasets/heart.csv')
 TargetNames.append('target')
 FileNameDataset.append('./datasets/categoricalfeatureencoding.csv')
 TargetNames.append('target')
-FileNameDataset.append('./datasets/sanfranciscocrime_split.csv')
-TargetNames.append('Category')
+#FileNameDataset.append('./datasets/sanfranciscocrime_split.csv')
+#TargetNames.append('Category')
 
 # Metafeatures
 post_processing_steps = {'mean': Mean(),
@@ -109,3 +154,8 @@ meta_functions_numerical = {'mean': basic_meta_functions.Mean(),
 
 model = autoBaggingClassifier('create')
 model.fit(FileNameDataset,TargetNames, meta_functions_categorical, meta_functions_numerical, post_processing_steps)
+
+#Avaliar Algoritmos
+#scoring = 'accuracy'
+#kfold = KFold(n_splits=10, random_state=0)
+#cv_results = cross_val_score(model, X, y, cv=kfold, scoring=scoring)
